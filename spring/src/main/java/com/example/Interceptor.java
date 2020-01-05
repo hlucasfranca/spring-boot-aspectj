35b6d31399514113381f715b9e3e5bbca11a7c97
Henrique Lucas França <hlucasfranca@gmail.com>
2020-01-05 19:52:29 -0300

    Testes

--- spring/src/main/java/com/example/Interceptor.java
+++ spring/src/main/java/com/example/Interceptor.java
@@ -27,23 +27,39 @@ public class Interceptor {
 		this.message = message;
 	}
 
-	@Around("execution(* *.*(..)) && within(com.example..*)")
-	public Object intercept(ProceedingJoinPoint joinPoint) throws Throwable {
-		Object result = joinPoint.proceed();
-		System.err.println(joinPoint.toShortString() + ": " + result);
-		return result;
+	@Around("within(com.example..*)")
+	public Object aroundAdvice(ProceedingJoinPoint proceedingJoinPoint) {
+		System.out.println("Before aroundAdvice method:"+proceedingJoinPoint.getSignature());
+		long methodStaringSystemTime = System.currentTimeMillis();
+		Object object = null;
+
+
+		Object[] signatureArgs = proceedingJoinPoint.getArgs();
+		for (Object signatureArg: signatureArgs) {
+			System.out.println("Arg: " + signatureArg);
+		}
+
+		try {
+			object = proceedingJoinPoint.proceed();
+		} catch (Throwable throwable) {
+			System.out.println("In around advice method:"+throwable.getMessage());
+		}
+		System.out.println("After aroundAdvice method:"+proceedingJoinPoint.getSignature());
+		long methodEndingSystemTime = System.currentTimeMillis();
+		System.out.println("Total execution time of aroundAdvice method is:"+(methodEndingSystemTime-methodStaringSystemTime)+" ms");
+		return object;
 	}
 
-	@Around("execution(* *.*(..)) && within(com.example..*)")
-	public Object another(ProceedingJoinPoint joinPoint) throws Throwable {
-		Object result = joinPoint.proceed();
-		System.err.println(joinPoint.toShortString() + ": " + result);
-		return result;
-	}
+//	@Around("execution(* *.*(..)) && within(com.example..*)")
+//	public Object another(ProceedingJoinPoint joinPoint) throws Throwable {
+//		Object result = joinPoint.proceed();
+//		System.err.println(joinPoint.toShortString() + ": " + result);
+//		return result;
+//	}
 
-	@EventListener
-	public void started(ContextRefreshedEvent event) {
-		System.err.println(message + ": " + event);
-	}
+//	@EventListener
+//	public void started(ContextRefreshedEvent event) {
+//		System.err.println(message + ": " + event);
+//	}
 
 }
--- spring/src/main/java/com/example/InterceptorApplication.java
+++ spring/src/main/java/com/example/InterceptorApplication.java
@@ -2,16 +2,33 @@ package com.example;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @SpringBootApplication
 @RestController
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class InterceptorApplication {
+
+	@Resource
+	private InterceptorApplication app;
 
 	@RequestMapping("/")
 	public String home() {
 		return "Hello World";
+	}
+
+	@GetMapping("/hi")
+	public void hi() {
+		app.teste("Fulano de ", "Tal");
+	}
+
+	public void teste(String nome1, String nome2){
+		System.out.println(nome1 + nome2);
 	}
 
 	public static void main(String[] args) {
